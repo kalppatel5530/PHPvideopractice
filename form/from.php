3243670918f2a432288da245172bb5eab9130ed8
kalppatel5530 <kalppatel.aorc@gmail.com>
2025-03-05 10:09:31 +0530

    updated

--- form/from.php
+++ form/from.php
@@ -1,3 +1,47 @@
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $errors = [];
+
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+    $gender = isset($_POST['gender']) ? $_POST['gender'] : '';
+    $city = trim($_POST['city']);
+    $bio = trim($_POST['bio']);
+
+    if (empty($name)) {
+        $errors[] = "Name is required.";
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email format.";
+    }
+
+    if (strlen($password) < 6) {
+        $errors[] = "Password must be at least 6 characters long.";
+    }
+
+    if (empty($gender)) {
+        $errors[] = "Gender is required.";
+    }
+
+    if (empty($city)) {
+        $errors[] = "City selection is required.";
+    }
+
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo "<p style='color:red;'>$error</p>";
+        }
+        echo "<a href='index.php'>Go Back</a>";
+    } else {
+        echo "<h3 style='color:green;'>Form Submitted Successfully</h3>";
+    }
+}
+?>
+
+
 <?php
 //print_r($_POST);
 if(isset($_POST['name'])){
