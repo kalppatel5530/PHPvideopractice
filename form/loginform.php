d3607f1f7973bc0d1b5470283f4f96fded5c1926
kalppatel5530 <kalppatel.aorc@gmail.com>
2025-03-05 10:59:37 +0530

    updated

--- form/loginform.php
+++ form/loginform.php
@@ -1,6 +1,27 @@
+
+<?php
+$nameerror="";
+$passworderror="";
+
+if(isset($_POST['submit'])){
+    $username= $_POST['username'];
+    $password = $_POST['password'];
+
+if(empty($username)){
+    $nameerror = "name is required";
+}
+
+}
+
+?>
+
+
+
+
+
 <?php
 //print_r($_POST);
-if(isset($_POST['name'])){
+if(isset($_POST['username'])){
     echo "user name is ". $_POST['username'];
     echo "<br>";
     echo "user password is ". $_POST['password'];
