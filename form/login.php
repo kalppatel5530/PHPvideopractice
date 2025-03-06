13e69231c23b3220cdc6e43b4ccc0f02592b3d2e
kalppatel5530 <kalppatel.aorc@gmail.com>
2025-03-06 11:03:04 +0530

    updated

--- form/login.php
+++ form/login.php
@@ -1,10 +1,16 @@
 <?php
 $nameerror="";
 $passworderror="";
+$websiteerror="";
+$emailerror="";
+$phoneerror="";
 
 if(isset($_POST['submit'])){
     $username= $_POST['username'];
     $password = $_POST['password'];
+    $website= $_POST['website'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
 
 if(empty($username)){
     $nameerror = "<br>name is required";
@@ -27,6 +33,32 @@ if (empty($password)) {
         $passworderror = "<br>At least one lowercase letter required";
     } elseif (!preg_match("#[A-Z]+#", $password)) {
         $passworderror = "<br>At least one uppercase letter required";
+    }
+}
+
+if (empty($website)){
+    $websiteerror = "<br> website is required";
+} else{
+    if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i" , $website)){
+        $websiteerror = "Invalid URL";
+    }
+}
+
+if(empty($email)){
+    $emailerror = "<br>email is required";
+}else{
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $emailerror="invalid email formate";
+    }
+}
+
+if(empty($phone)){
+    $phoneerror="<br>phone no is required";
+}else{
+    if(strlen($phone)<10){
+        $phoneerror="invalid number";
+    }elseif (!preg_match("/^[0-9]{10}$/", $phone)) {
+        $phoneerror = "Phone number should contain  10 digits";
     }
 }
 
@@ -56,6 +88,17 @@ if (empty($password)) {
         <input type="password" name="password" >
         <span style="color:red;"><?php echo $passworderror ?></span>
         <br><br>
+        <label>Website:</label>
+        <input type="text" name="website">
+        <span style="color:red;"><?php echo $websiteerror ?></span>
+        <br><br>
+        <label>Email:</label>
+        <input type="email" name="email">
+        <br><br>
+        <label>Phone:</label>
+        <input type="text" name="phone">
+        <span style="color:red;"><?php echo $phoneerror ?></span>
+        <br><br>
 
         <button type="submit" name="submit">Login</button>
        
@@ -63,10 +106,16 @@ if (empty($password)) {
 
     <?php
 
-if(isset($_POST['username']) && empty($nameerror) && empty($passworderror)){
+if(isset($_POST['username']) && empty($nameerror) && empty($passworderror) && empty($websiteerror)&& empty($phoneerror)){
     echo "username is " . $_POST["username"];
     echo "<br>";
      echo "password is " .$_POST["password"];
+     echo "<br>";
+     echo "website is " .$_POST['website'];
+     echo "<br>";
+     echo "email is " .$_POST['email'];
+     echo "<br>";
+     echo "phone number is " .$_POST['phone'];
 }
 
 ?> 
