60d6eb417397e0cd8a0a204569022c96d3b56c0a
kalppatel5530 <kalppatel.aorc@gmail.com>
2025-03-11 11:59:04 +0530

    updated

--- crud/display.php
+++ crud/display.php
@@ -1,3 +1,18 @@
+ <html>
+    <head>
+        <title>Display</title>
+        <style>
+            body{
+                background : #d071f9;
+            }
+            table{
+                background-color: white;
+            }
+        </style>
+    </head>
+    </html>
+ 
+ 
  <?php
 include("connection.php"); 
 error_reporting(0);
@@ -6,15 +21,41 @@ $data = mysqli_query($conn , $query);
 
 $total= mysqli_num_rows($data);
 
-$result= mysqli_fetch_assoc($data);
 
-echo $result['first_name']." ".$result['last_name']." ".$result['gender']." ".$result['email']." ".$result['phone']." ".$result['address'];
+
+
 // echo $total;
 
 if($total != 0)
 {
-// echo "table has records";
+    ?>
+    <h2 align="center"><mark>Displaying all records</mark></h2>
+<table border="1" cellspacing="7" width="85%" align="center">
+    <tr>
+    <th width="10%">First name</th>
+    <th width="10%">Last name</th>
+    <th width="10%">gender</th>
+    <th width="20%">email</th>
+    <th width="10%">phone</th>
+    <th width="25%">address</th>
+</tr>
+
+    <?php
+while($result= mysqli_fetch_assoc($data)){
+echo " <tr>
+    <td>".$result['first_name']."</td>
+    <td>".$result['last_name']."</td>
+    <td>".$result['gender']."</td>
+    <td>".$result['email']."</td>
+    <td>".$result['phone']."</td>
+    <td>".$result['address']."</td>
+</tr>";
+
+   
+}
 }else{
     echo "no record found";
 }
-?>  
+?>
+</table>
+</html>  
